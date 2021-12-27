5260d885a38467843d84320bc2af36295e10efd3
akro <akro>
2021-12-27 17:44:19 +0900

    first stab at a model

--- tests/test_batches.py
+++ tests/test_batches.py
@@ -1,12 +1,12 @@
 import unittest
 from datetime import date
 
-from model import Batch, OrderLine
+from src.model import OrderLine, Batch
 
 
 class MyTestCase(unittest.TestCase):
     def test_allocating_to_a_batch_reduces_the_available_quantity(self):
-        batch = Batch("batch-001", "SMALL-TABLE", qty=20, etc=date.today())
+        batch = Batch("batch-001", "SMALL-TABLE", qty=20, eta=date.today())
         line = OrderLine('order-ref', "SMALL-TABLE", 2)
 
         batch.allocate(line)
