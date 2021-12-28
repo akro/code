538b255bf5a5cd6549368a3448236278602ec9be
akro <akro>
2021-12-28 15:18:00 +0900

    first cut of flask app

---
+++ src/config.py
@@ -0,0 +1,3 @@
+def in_memory_db():
+    return "sqlite:///:memory:"
+
---
+++ src/flask_app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+import config
+import model
+import orm
+import repository
+
+orm.start_mappers()
+get_session = sessionmaker(bind=create_engine(config.in_memory_db()))
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "i"
+
+@app.route("/allocate", methods=["POST"])
+def allocate_endpoint():
+    session = get_session()
+    batches = repository.SqlAlchemyRepository(session).list()
+    line = model.OrderLine(
+        request.json["orderid"], request.json["sku"], request.json["qty"],
+    )
+
+    batchref = model.allocate(line, batches)
+
+    return {"batchref": batchref}, 201
